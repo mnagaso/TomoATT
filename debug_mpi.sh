20cd8e0f3222614193a2d9602a12f2beaaef99a5
Masaru Nagaso <mnsaru22@gmail.com>
2023-01-05 00:48:04 +0800

    modify exec path

--- debug_mpi.sh
+++ debug_mpi.sh
@@ -3,14 +3,14 @@
 # general mpi debugging
 
 # instantly start the debugger
-#mpirun --oversubscribe -np $1 xterm -e gdb  -ex run --args ../../build/TOMOATT -v -i $2
+mpirun --oversubscribe -np $1 xterm -e gdb  -ex run --args ../../build/bin/TOMOATT -v -i $2
 # for break point insertion
-mpirun --oversubscribe -np $1 xterm -e gdb  --args ../../build/TOMOATT -i $2
+#mpirun --oversubscribe -np $1 xterm -e gdb  --args ../../build/bin/TOMOATT -i $2
 
 # valgrind memory leak check
-#mpirun --oversubscribe -np $1 valgrind --log-file="log_val" --leak-check=yes --track-origins=yes ../../build/TOMOATT -i $2
+#mpirun --oversubscribe -np $1 valgrind --log-file="log_val" --leak-check=yes --track-origins=yes ../../build/bin/TOMOATT -i $2
 
 # cuda debug
-#mpirun --oversubscribe -np $1 xterm -e cuda-gdb  -ex run --args ../../build/TOMOATT $2
+#mpirun --oversubscribe -np $1 xterm -e cuda-gdb  -ex run --args ../../build/bin/TOMOATT $2
 # nvprof
-#nvprof mpirun --oversubscribe -np 1 ../../build/TOMOATT $2
+#nvprof mpirun --oversubscribe -np 1 ../../build/bin/TOMOATT $2
