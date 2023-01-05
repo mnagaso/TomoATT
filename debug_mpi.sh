962c70b0015b2cfb30032da0d9264e7ad17d4f6f
M.Nagaso <mnsaru22@gmail.com>
2023-01-05 14:10:03 +0800

    add tui

--- debug_mpi.sh
+++ debug_mpi.sh
@@ -3,9 +3,12 @@
 # general mpi debugging
 
 # instantly start the debugger
-mpirun --oversubscribe -np $1 xterm -e gdb  -ex run --args ../../build/bin/TOMOATT -v -i $2
+#mpirun --oversubscribe -np $1 xterm -e gdb  -ex run --args ../../build/bin/TOMOATT -v -i $2
 # for break point insertion
-#mpirun --oversubscribe -np $1 xterm -e gdb  --args ../../build/bin/TOMOATT -i $2
+mpirun --oversubscribe -np $1 xterm -e gdb  --args ../../build/bin/TOMOATT -i $2
+# tui mode
+#mpirun --oversubscribe -np $1 xterm -e gdb --tui --args ../../build/bin/TOMOATT -i $2
+
 
 # valgrind memory leak check
 #mpirun --oversubscribe -np $1 valgrind --log-file="log_val" --leak-check=yes --track-origins=yes ../../build/bin/TOMOATT -i $2
