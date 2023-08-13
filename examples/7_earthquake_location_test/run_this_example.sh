09952696d9f06fb6245477e404b6bf6fb79b0454
JingChen-Thu <cjcjyu597460474@163.com>
2023-08-13 17:49:16 +0800

    am

--- examples/7_earthquake_location_test/run_this_example.sh
+++ examples/7_earthquake_location_test/run_this_example.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-# python make_test_model.py
+python make_test_model.py
 
 mkdir OUTPUT_FILES
 
 # compute synthetic traveltime in the ckb model
-mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_signal.yml
+mpirun -n 2 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_signal.yml
 
-# python generate_obs_src_rec_data.ipynb
+python generate_obs_src_rec_data.py
 
 # run location using absolute traveltime data
-mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_abs.yml
+mpirun -n 2 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_abs.yml
 
 # run location using common receiver differential traveltime data
-mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_cr.yml
+mpirun -n 2 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_cr.yml
 
 # run location using both data
-mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_abs_cr.yml
+mpirun -n 2 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params/input_params_reloc_abs_cr.yml
 
-# python compare_location_result.ipynb
+python compare_location_result.py
 
 
