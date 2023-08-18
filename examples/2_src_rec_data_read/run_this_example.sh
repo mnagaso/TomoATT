44a1d1c4345cf1cb92f4df03586455239b75ce03
Masaru Nagaso <mnsaru22@gmail.com>
2023-08-18 21:55:35 +0800

    call correct script to check

--- examples/2_src_rec_data_read/run_this_example.sh
+++ examples/2_src_rec_data_read/run_this_example.sh
@@ -14,7 +14,7 @@ mpirun -n 1 --allow-run-as-root ../../build/bin/TOMOATT -i input_params/input_pa
 # run for swap parallel
 mpirun -n 2 --allow-run-as-root ../../build/bin/TOMOATT -i input_params/input_params_swap_parallel.yml
 
-python compare_src_rec.py
+#python compare_src_rec.py
 
 # then final model can be plot by e.g. check_3d_out.ipynb
 #paraview OUTPUT_FILES/out_data_sim.xmf
