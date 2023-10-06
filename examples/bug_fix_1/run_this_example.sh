9bbc5f570a59835aa13945ad6ce63b299e24ae42
Masaru Nagaso <mnsaru22@gmail.com>
2023-10-06 18:14:51 +0800

    use appropriate files

--- examples/bug_fix_1/run_this_example.sh
+++ examples/bug_fix_1/run_this_example.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-mkdir OUTPUT_FILES
-
 # compute synthetic traveltime in the ckb model
-# mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params_signal.yml
+mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params_signal.yml
 
 mpirun -n 8 --allow-run-as-root --oversubscribe ../../build/bin/TOMOATT -i input_params_inv_abs_cr_reloc_abs_cr.yml
 
