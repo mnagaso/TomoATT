50aa447864389019b27c26b14730e927d7a270e8
Masaru Nagaso <mnsaru22@gmail.com>
2024-11-07 15:52:14 -0700

    modify script

--- test/inversion_small/run_this_example.sh
+++ test/inversion_small/run_this_example.sh
@@ -6,6 +6,8 @@ python make_test_model.py
 nproc_sweep=$1
 # get number of processes for domain decomposition
 nproc_dd=$2
+# calculate the total number of processes
+nproc_total=$((nproc_sweep*nproc_dd*nproc_dd*nproc_dd))
 
 # modify the input_params_pre.yml file
 #  nproc_sub: (integer) # number of processors for sweep parallelization (parallel the fast sweep method)
@@ -19,10 +21,10 @@ sed -i "s/nproc_sub: [0-9]\+/nproc_sub: $nproc_sweep/g" input_params.yml
 sed -i "s/ndiv_rtp: \[[0-9]\+, [0-9]\+, [0-9]\+\]/ndiv_rtp: \[$nproc_dd, $nproc_dd, $nproc_dd\]/g" input_params.yml
 
 # run for preparing true travel times
-mpirun --oversubscribe -n 2 ../../build/bin/TOMOATT -i input_params_pre.yml
+mpirun --oversubscribe -n $nproc_total ../../build/bin/TOMOATT -i input_params_pre.yml
 
 # run for inversion
-mpirun --oversubscribe -n 2 ../../build/bin/TOMOATT -i input_params.yml
+mpirun --oversubscribe -n $nproc_total ../../build/bin/TOMOATT -i input_params.yml
 #mpirun -n 8 ../../build/bin/TOMOATT -i input_params_flexible_inv_grid.yml
 
 # then final model can be plot by e.g. check_3d_out.ipynb
