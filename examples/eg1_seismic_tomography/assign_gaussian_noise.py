5547a69f7884b809604a967066ecd59a51748daa
xumi1993 <gomijianxu@gmail.com>
2024-11-08 23:26:50 -0500

    comment out `assign_gaussian_noise`

--- examples/eg1_seismic_tomography/assign_gaussian_noise.py
+++ examples/eg1_seismic_tomography/assign_gaussian_noise.py
@@ -1,31 +1,29 @@
-# %%
-# from pytomoatt.src_rec import SrcRec
+from pytomoatt.src_rec import SrcRec
 
-# def assign_noise_to_src_rec_file(in_fname, out_fname, noise_level=0.1):
-#     sr = SrcRec.read(in_fname)
-#     sr.add_noise(noise_level)
-#     sr.write(out_fname)
+def assign_noise_to_src_rec_file(in_fname, out_fname, noise_level=0.1):
+    sr = SrcRec.read(in_fname)
+    sr.add_noise(noise_level)
+    sr.write(out_fname)
 
 
-# if __name__ == "__main__":
-#     in_fname = "OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward.dat" # input source receiver file
-#     out_fname = "OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward_noisy.dat" # output source receiver file
-#     sigma = 0.1 # noise level in seconds
-#     assign_noise_to_src_rec_file(in_fname, out_fname, noise_level=sigma)
+if __name__ == "__main__":
+    in_fname = "OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward.dat" # input source receiver file
+    out_fname = "OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward_noisy.dat" # output source receiver file
+    sigma = 0.1 # noise level in seconds
+    assign_noise_to_src_rec_file(in_fname, out_fname, noise_level=sigma)
 
-# %%
-import sys
-sys.path.append('../utils')
-import functions_for_data as ffd
+# import sys
+# sys.path.append('../utils')
+# import functions_for_data as ffd
 
-# read src_rec_file
-ev, st = ffd.read_src_rec_file('OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward.dat')
+# # read src_rec_file
+# ev, st = ffd.read_src_rec_file('OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward.dat')
 
-# assign gaussian noise to the data
-# ffd.assign_gaussian_noise(ev, 0.1)
+# # assign gaussian noise to the data
+# # ffd.assign_gaussian_noise(ev, 0.1)
 
-# output file
-ffd.write_src_rec_file('OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward_noisy.dat', ev, st)
+# # output file
+# ffd.write_src_rec_file('OUTPUT_FILES/OUTPUT_FILES_signal/src_rec_file_forward_noisy.dat', ev, st)
 
 # # perturbate earthquake locations
 # lat_pert = 0.1 # degrees
--- examples/eg1_seismic_tomography/prepare_input_files.py
+++ examples/eg1_seismic_tomography/prepare_input_files.py
@@ -1,34 +1,34 @@
-# %%
 # download src_ref_files from Zenodo
 import os
 import requests
+import numpy as np
+import sys
+try:
+    from pytomoatt.model import ATTModel
+    from pytomoatt.checkerboard import Checker
+except:
+    print("ATTModel not found. Please install pytomoatt first."
+          "See https://tomoatt.github.io/PyTomoATT/installation.html for details.")
+    sys.exit(1)
+
 
 url = 'https://zenodo.org/records/14053821/files/src_rec_config.dat?download=1'
 
 path = "1_src_rec_files/src_rec_config.dat"
 
-# check file existence
-if not os.path.exists(path):
-    try:
-        os.mkdir("1_src_rec_files")
-    except:
-        pass
-    print("Downloading src_rec_config.dat from Zenodo...")
-    response = requests.get(url, stream=True)
-    with open(path, 'wb') as out_file:
-        out_file.write(response.content)
-    print("Download complete.")
-else:
-    print("src_rec_config.dat already exists.")
+def download_src_rec():
+    # check file existence
+    if not os.path.exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        print("Downloading src_rec_config.dat from Zenodo...")
+        response = requests.get(url, stream=True)
+        with open(path, 'wb') as out_file:
+            out_file.write(response.content)
+        print("Download complete.")
+    else:
+        print("src_rec_config.dat already exists.")
 
-# %%
 # prepare initial model
-
-import numpy as np
-import os
-import h5py
-from pytomoatt.model import ATTModel
-
 
 class BuildInitialModel():
     def __init__(self, par_file="./3_input_params/input_params_signal.yaml", output_dir="2_models"):
@@ -50,37 +50,22 @@ class BuildInitialModel():
             else:
                 self.am.vel[ir, :, :] = 8.0
 
-if __name__ == "__main__":
-    try:
-        os.mkdir("2_models")
-    except:
-        pass
-    bim = BuildInitialModel()
-    bim.build_initial_model()
-    bim.am.write('{}/model_init_N{:d}_{:d}_{:d}.h5'.format(bim.output_dir, *bim.am.n_rtp))
-
-# %%
 class BuildCkbModel():
     def __init__(self, par_file="./3_input_params/input_params_signal.yaml", output_dir="2_models"):
         """
         Build initial model for tomography inversion
         """
+        # read initial model
+        self.im = ATTModel.read(f'{output_dir}/model_init_N61_61_61.h5', par_file)
+        # create instance of cbk model
         self.am = ATTModel(par_file)
         self.output_dir = output_dir
 
-    def build_ckb_model(self):
+    def build_ckb_model(self, vel_pert=0.2, ani_pert=0.1):
         """
         Build ckb model for tomography inversion
         """
-        vel_pert = 0.2
-        ani_pert = 0.1
-
-        # read initial model
-        model_fname = "2_models/model_init_N61_61_61.h5"   # 检测板反演的最终模型
-        with h5py.File(model_fname, 'r') as fmodel:
-            vel_init    = np.array(fmodel['vel'])
-
-        self.am.vel = vel_init
+        self.am.vel = self.im.vel.copy()
 
         for ir, dep in enumerate(self.am.depths):
             for ip, lat in enumerate(self.am.latitudes):
@@ -95,17 +80,37 @@ class BuildCkbModel():
                         sigma = 0.0
                         psi   = 0.0
 
-                    self.am.vel[ir,it,ip]   = vel_init[ir,it,ip] * (1.0 + vel_pert * sigma)
+                    self.am.vel[ir,it,ip]   = self.im.vel[ir,it,ip] * (1.0 + vel_pert * sigma)
                     self.am.xi[ir,it,ip]    = ani_pert * abs(sigma) * np.cos(2*psi) 
                     self.am.eta[ir,it,ip]   = ani_pert * abs(sigma) * np.sin(2*psi) 
                     self.am.zeta[ir,it,ip]  = 0.0
 
+#==============================================================================
+# Checkerboard model using PyTomoATT v0.2.5 (current devel version)
+#==============================================================================
+# def build_ckb_model(output_dir="2_models"):
+#     cbk = Checker(f'{output_dir}/model_init_N61_61_61.h5', par_file="./3_input_params/input_params_signal.yaml")
+#     cbk.checkerboard(
+#         n_pert_x=2, n_pert_y=2, n_pert_z=2,
+#         pert_vel=0.2, pert_ani=0.1, ani_dir=60.0,
+#         lim_x=[0.5, 1.5], lim_y=[0.5, 1.5], lim_z=[0, 40]
+#     )
+#     cbk.write(f'{output_dir}/model_ckb_N61_61_61.h5')
+#==============================================================================
+
+
 
 if __name__ == "__main__":
-    try:
-        os.mkdir("2_models")
-    except:
-        pass
+    # download src_rec_config.dat
+    download_src_rec()
+
+    # build initial model
+    os.makedirs("2_models", exist_ok=True)
+    bim = BuildInitialModel()
+    bim.build_initial_model()
+    bim.am.write('{}/model_init_N{:d}_{:d}_{:d}.h5'.format(bim.output_dir, *bim.am.n_rtp))
+
+    # build ckb model
     bim = BuildCkbModel()
     bim.build_ckb_model()
     bim.am.write('{}/model_ckb_N{:d}_{:d}_{:d}.h5'.format(bim.output_dir, *bim.am.n_rtp))
